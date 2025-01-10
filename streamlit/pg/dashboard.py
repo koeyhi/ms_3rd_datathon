6c30e3c703cf1bc48037ef66590afc71b0fe2bb9
koeyhi <104114941+koeyhi@users.noreply.github.com>
2025-01-10 11:33:11 +0900

    change path

--- streamlit/pg/dashboard.py
+++ streamlit/pg/dashboard.py
@@ -2,10 +2,10 @@ import pandas as pd
 import streamlit as st
 
 st.set_page_config(page_title="Dashboard", layout="wide")
-DATA_PATH = "../LoLesports_data/"
+DATA_PATH = "data/"
 
-df = pd.read_csv(f"{DATA_PATH}/2022_LoL_esports_match_data_from_OraclesElixir.csv")
+df = pd.read_csv(f"{DATA_PATH}featured_data.csv")
 
-st.header("2022 리그 오브 레전드 경기 데이터")
+st.header("리그 오브 레전드 경기 데이터")
 with st.expander("데이터 보기"):
     st.write(df)
