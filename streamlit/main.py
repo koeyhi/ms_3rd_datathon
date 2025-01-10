9a7c234621490d27c3db47d84902dcd613c15bfa
koeyhi <104114941+koeyhi@users.noreply.github.com>
2025-01-10 11:43:07 +0900

    path check

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import os
+import streamlit as st
+
+st.write(f"현재 작업 디렉토리: {os.getcwd()}")
+st.write("디렉토리 내 파일 목록:")
+st.write(os.listdir())
 
 pg_list = [
     st.Page("pg/home.py", title="홈"),
