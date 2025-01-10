80bc06c7ccac136cae5a76ea3fb2a1e529ee0b61
koeyhi <104114941+koeyhi@users.noreply.github.com>
2025-01-10 12:14:20 +0900

    delete check code

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,14 +1,4 @@
 import streamlit as st
-import subprocess
-import sys
-
-print(sys.version)
-
-try:
-    subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
-    subprocess.run([sys.executable, "-m", "pip", "install", "joblib"], check=True)
-except subprocess.CalledProcessError as e:
-    print(f"패키지 설치 실패: {e}")
 
 pg_list = [
     st.Page("pg/home.py", title="홈"),
