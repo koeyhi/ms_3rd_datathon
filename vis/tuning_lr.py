dab1ab4d085c55bd5918eaa3a39dfff1588b4108
koeyhi <104114941+koeyhi@users.noreply.github.com>
2025-01-17 11:20:41 +0900

    wandb 시도(보류)

---
+++ vis/tuning_lr.py
@@ -0,0 +1,56 @@
+import wandb
+import pandas as pd
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.linear_model import LogisticRegression
+import yaml
+
+
+def train_lr(config_defaults=None):
+    with wandb.init(config=config_defaults):
+        config = wandb.config
+
+        train_x = pd.read_csv("vis/data/train_x.csv")
+        valid_x = pd.read_csv("vis/data/valid_x.csv")
+        test_x = pd.read_csv("vis/data/test_ft.csv")
+        train_y = pd.read_csv("vis/data/train_y.csv")
+        valid_y = pd.read_csv("vis/data/valid_y.csv")
+        test_y = pd.read_csv("vis/data/test_target.csv")
+
+        model = LogisticRegression(
+            C=config.C,
+            penalty=config.penalty,
+            solver=config.solver,
+            max_iter=config.max_iter,
+            random_state=42,
+        )
+
+        model.fit(train_x, train_y)
+
+        valid_pred = model.predict(valid_x)
+        valid_acc = accuracy_score(valid_y, valid_pred)
+        valid_roc_auc = roc_auc_score(valid_y, model.predict_proba(valid_x)[:, 1])
+
+        train_ft = pd.concat([train_x, valid_x])
+        train_target = pd.concat([train_y, valid_y])
+
+        model.fit(train_ft, train_target)
+
+        test_pred = model.predict(test_x)
+        test_acc = accuracy_score(test_y, test_pred)
+        test_roc_auc = roc_auc_score(test_y, model.predict_proba(test_x)[:, 1])
+
+        wandb.log(
+            {
+                "valid_accuracy": valid_acc,
+                "valid_roc_auc": valid_roc_auc,
+                "test_accuracy": test_acc,
+                "test_roc_auc": test_roc_auc,
+            }
+        )
+
+
+if __name__ == "__main__":
+    with open("vis/sweep_lr.yaml", "r") as file:
+        sweep_config = yaml.safe_load(file)
+    sweep_id = wandb.sweep(sweep_config, project="my_model_experiments")
+    wandb.agent(sweep_id, function=train_lr, count=10)
---
+++ vis/tuning_rf.py
@@ -0,0 +1,58 @@
+import wandb
+import pandas as pd
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+import yaml
+
+
+def train_rf(config_defaults=None):
+    with wandb.init(config=config_defaults):
+        config = wandb.config
+
+        train_x = pd.read_csv("vis/data/train_x.csv")
+        valid_x = pd.read_csv("vis/data/valid_x.csv")
+        test_x = pd.read_csv("vis/data/test_ft.csv")
+        train_y = pd.read_csv("vis/data/train_y.csv")
+        valid_y = pd.read_csv("vis/data/valid_y.csv")
+        test_y = pd.read_csv("vis/data/test_target.csv")
+
+        model = RandomForestClassifier(
+            n_estimators=config.n_estimators,
+            max_depth=config.max_depth,
+            min_samples_split=config.min_samples_split,
+            min_samples_leaf=config.min_samples_leaf,
+            max_features=config.max_features,
+            bootstrap=config.bootstrap,
+            random_state=42,
+        )
+
+        model.fit(train_x, train_y)
+
+        valid_pred = model.predict(valid_x)
+        valid_acc = accuracy_score(valid_y, valid_pred)
+        valid_roc_auc = roc_auc_score(valid_y, model.predict_proba(valid_x)[:, 1])
+
+        train_ft = pd.concat([train_x, valid_x])
+        train_target = pd.concat([train_y, valid_y])
+
+        model.fit(train_ft, train_target)
+
+        test_pred = model.predict(test_x)
+        test_acc = accuracy_score(test_y, test_pred)
+        test_roc_auc = roc_auc_score(test_y, model.predict_proba(test_x)[:, 1])
+
+        wandb.log(
+            {
+                "valid_accuracy": valid_acc,
+                "valid_roc_auc": valid_roc_auc,
+                "test_accuracy": test_acc,
+                "test_roc_auc": test_roc_auc,
+            }
+        )
+
+
+if __name__ == "__main__":
+    with open("vis/sweep_rf.yaml", "r") as file:
+        sweep_config = yaml.safe_load(file)
+    sweep_id = wandb.sweep(sweep_config, project="my_model_experiments")
+    wandb.agent(sweep_id, function=train_rf, count=10)
