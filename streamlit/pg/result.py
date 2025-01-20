53b80a2469a63b1103f518b421e4ddb0a17f58d3
koeyhi <104114941+koeyhi@users.noreply.github.com>
2025-01-20 11:09:26 +0900

    선수 데이터 상관관계 제외

--- streamlit/pg/result.py
+++ streamlit/pg/result.py
@@ -44,39 +44,39 @@ st.pyplot(fig)
 
 # ------------------------------------------
 # 1. 데이터 로드 (경로 지정)
-# 사용자 데이터 경로 지정
-player_df = pd.read_csv(f"{DATA_PATH}player.csv")
+# # 사용자 데이터 경로 지정
+# player_df = pd.read_csv(f"{DATA_PATH}player.csv")
 
-player_df = player_df[player_df["year"] == 2024]  # 2024년 데이터만 선택
+# player_df = player_df[player_df["year"] == 2024]  # 2024년 데이터만 선택
 
-st.write(f"### 2024년 선수 데이터 (총 {player_df.shape[0]}개)")
-st.dataframe(player_df.head())
+# st.write(f"### 2024년 선수 데이터 (총 {player_df.shape[0]}개)")
+# st.dataframe(player_df.head())
 
-# 3. 문자열 타입 컬럼 제거
-string_columns = player_df.select_dtypes(include=["object"]).columns
-if not string_columns.empty:
-    player_df = player_df.drop(columns=string_columns)
+# # 3. 문자열 타입 컬럼 제거
+# string_columns = player_df.select_dtypes(include=["object"]).columns
+# if not string_columns.empty:
+#     player_df = player_df.drop(columns=string_columns)
 
-# 4. 특정 키워드 포함 컬럼 제거
-keywords = ["opp", "10", "15"]
-columns_to_drop = [
-    col for col in player_df.columns if any(keyword in col for keyword in keywords)
-]
-if columns_to_drop:
-    player_df = player_df.drop(columns=columns_to_drop)
+# # 4. 특정 키워드 포함 컬럼 제거
+# keywords = ["opp", "10", "15"]
+# columns_to_drop = [
+#     col for col in player_df.columns if any(keyword in col for keyword in keywords)
+# ]
+# if columns_to_drop:
+#     player_df = player_df.drop(columns=columns_to_drop)
 
-# 5. 상관관계 분석
-correlation = player_df.corr()  # 2024년 데이터로 전체 상관행렬 계산
-result_correlation = correlation["result"].sort_values(
-    ascending=False
-)  # result와의 상관관계만 정렬
+# # 5. 상관관계 분석
+# correlation = player_df.corr()  # 2024년 데이터로 전체 상관행렬 계산
+# result_correlation = correlation["result"].sort_values(
+#     ascending=False
+# )  # result와의 상관관계만 정렬
 
-st.write("#### Result와의 상관계수")
-st.dataframe(result_correlation)
+# st.write("#### Result와의 상관계수")
+# st.dataframe(result_correlation)
 
-# 히트맵 시각화
-st.write("### 2024년 선수 승리 히트맵")
-fig, ax = plt.subplots(figsize=(10, 8))
-sns.heatmap(correlation, annot=False, cmap="coolwarm", cbar=True, ax=ax)
-plt.title("2024년 Correlation Matrix")
-st.pyplot(fig)
+# # 히트맵 시각화
+# st.write("### 2024년 선수 승리 히트맵")
+# fig, ax = plt.subplots(figsize=(10, 8))
+# sns.heatmap(correlation, annot=False, cmap="coolwarm", cbar=True, ax=ax)
+# plt.title("2024년 Correlation Matrix")
+# st.pyplot(fig)
