50ee591d530bd88a4d358915f13adc69095a9df7
koeyhi <104114941+koeyhi@users.noreply.github.com>
2025-01-20 14:30:45 +0900

    한글폰트 테스트

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+
+font_path = "fonts/NanumGothic-Regular.ttf"
+
+font_manager.fontManager.addfont(font_path)
+plt.rcParams["font.family"] = font_manager.FontProperties(fname=font_path).get_name()
+plt.rcParams["axes.unicode_minus"] = False
 
 pg_list = [
     st.Page("pg/home.py", title="홈"),
